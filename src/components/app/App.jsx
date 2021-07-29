97ef9a2499273e1de504bd09ebd35a32ff882b15
Tyler Farris <Tylerpfarris@gmail.com>
2021-07-28 21:00:45 -0600

    working on the material ui delete functionality

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Form from '../Form';
 
 import Table from '../Table';
+import TableMat from '../TableMat';
 
 export default function App() {
   return (
     <div>
       <Form />
       <Table />
+      <TableMat />
     </div>
   );
 }
