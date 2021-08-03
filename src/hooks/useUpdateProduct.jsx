09e6c3410385c7bc0b6b4917016a58925aa3dcdd
Tyler Farris <Tylerpfarris@gmail.com>
2021-08-03 14:04:17 -0600

    fixing prop types

--- src/hooks/useUpdateProduct.jsx
+++ src/hooks/useUpdateProduct.jsx
@@ -22,8 +22,8 @@ export const useUpdateProduct = (products, setProducts) => {
     return productImgRef.update({
       title: product.title,
       description: product.description,
-      price: Number(product.price),
-      quantity: Number(product.quantity),
+      price: product.price,
+      quantity: product.quantity,
       img: uploadImg,
     });
   };
@@ -33,14 +33,14 @@ export const useUpdateProduct = (products, setProducts) => {
     const pro = products.map((row, j) =>
       j === i ? { ...row, [name]: value } : row
     );
-    
+
     setUpdatedProduct({
       [name]: value,
       img: name === 'img' ? value.file : product.img,
     });
     setProducts(pro);
   };
-  
+
   const handleStopEdit = (product) => {
     updateProduct(product.id, updatedProduct);
     setEditIdx(-1);
